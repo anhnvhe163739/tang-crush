2bb12ba1ee3713521b2610cb5b524420b945efbb
anhnvhe163739 <97141165+anhnvhe163739@users.noreply.github.com>
2022-11-12 00:06:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'đồ ngốc',
-    title: 'Nguyễn Hồng Nhung là đồ khùm đin😙',
+    btnIntro: 'đồ ngốc đáng iu',
+    title: 'Nguyễn Thị Linh Tuyền là em bé cụa anh😙',
     desc: 'Tuy hâm dở hấp hấp nma iu mình anh hoy đúng hong ',
-    btnYes: 'Chị iu anh vl<33',
-    btnNo: 'Không, em trai à :3',
+    btnYes: 'Bé iu anh vl<33',
+    btnNo: 'Không, em chỉ coi anh là anh trai mưa thui à :<',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'tại vì anh cgi cũng 10 điểm =))))',
+    reply: 'tại vì em bị điên á=))))',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối 30 8h anh qua đón nhé công chúa.',
+    messDesc: 'hum nào anh qua đón em đi shopping nhé công chúa bé bỏng cụa anh.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100021646965755' 
 }
